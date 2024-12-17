9a9aad19e9eaea187658734d60f073b9abfab387
tomi <journeyneverends0510@gmail.com>
2024-12-17 12:42:28 +0900

    データベースの値を取り込み表示する

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[8.0].define(version: 2024_12_17_002617) do
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
