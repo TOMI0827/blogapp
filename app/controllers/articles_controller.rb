56a6c2e2d12febc9cdf6d85208cd410470bdecaa
tomi <journeyneverends0510@gmail.com>
2025-01-19 21:39:49 +0900

    細かい修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
 
     private
     def article_params
-        params.require(:article).permit(:title, :conten, :eyecatch)
+        params.require(:article).permit(:title, :content, :eyecatch)
     end
 
     def set_article
